2758ff0b49081459163e77285ab426c5454c3860
umaidashi <umaidashi18@gmail.com>
2025-02-13 16:04:20 +0900

    feat: add condition for Loca

--- greet-no-di/main.go
+++ greet-no-di/main.go
@@ -6,13 +6,25 @@ import (
 	"net/http"
 )
 
+const EN = "en"
+const JA = "ja"
+const KR = "kr"
+
+const LOC = EN
+
 func main() {
 	handler := Handler{}
 
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: &handler,
+		Handler: nil,
 	}
+
+	// curl http://localhost:8080/greet
+	http.HandleFunc("/greet", handler.Greet)
+
+	// curl http://localhost:8080/greetWithName\?name=hoge
+	http.HandleFunc("/greetWithName", handler.GreetWithName)
 
 	fmt.Println("Server Started")
 	server.ListenAndServe()
@@ -20,11 +32,30 @@ func main() {
 
 type Handler struct{}
 
-func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// greet.GreetInHangeul()
-	// greet.GreetInJapanese()
+func (h *Handler) Greet(w http.ResponseWriter, r *http.Request) {
+	var msg string
+	if LOC == EN {
+		msg = greet.GreetInEnglish()
+	} else if LOC == JA {
+		msg = greet.GreetInJapanese()
+	} else if LOC == KR {
+		msg = greet.GreetInHangeul()
+	}
 
-	greet := greet.GreetInEnglish()
+	fmt.Fprintln(w, msg)
+}
 
-	fmt.Fprintln(w, greet)
+func (h *Handler) GreetWithName(w http.ResponseWriter, r *http.Request) {
+	var msg string
+	if LOC == EN {
+		msg = greet.GreetInEnglish()
+	} else if LOC == JA {
+		msg = greet.GreetInJapanese()
+	} else if LOC == KR {
+		msg = greet.GreetInHangeul()
+	}
+
+	name := r.URL.Query().Get("name")
+
+	fmt.Fprintln(w, fmt.Sprintf("%s, %s", msg, name))
 }
